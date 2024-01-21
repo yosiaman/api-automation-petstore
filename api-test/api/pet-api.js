207fd907f084af935e25f5ec217b40889d5d7c85
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-01-21 23:59:58 +0700

    add all pet api

--- api-test/api/pet-api.js
+++ api-test/api/pet-api.js
@@ -6,6 +6,47 @@ const getPetByPetId = (petId) => request.get(`/pet/${petId}`)
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json');
 
+const addNewPet = (requestBody) => request.post(`/pet`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send(requestBody);
+
+const uploadPetImage = (petId, requestBody) => request.post(`/pet/${petId}/uploadImage`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send(requestBody);
+
+const updatePet = (requestBody) => request.put(`/pet`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send(requestBody);
+
+const updatePetPartial = (petId, requestBody) => request.post(`/pet/${petId}`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send(requestBody);
+
+/* 
+[PET STATUS]
+- available
+- pending
+- sold
+*/
+const findPetByStatus = (petStatus) => request.get(`/pet/findByStatus`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .query(petStatus);
+
+const deletePetByPetId = (petId) => request.delete(`/pet/${petId}`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+
 module.exports = {
-    getPetByPetId
+    getPetByPetId,
+    findPetByStatus,
+    addNewPet,
+    updatePet,
+    updatePetPartial,
+    uploadPetImage,
+    deletePetByPetId
 }
