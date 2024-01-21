63fde49a8d2160f7cd6870ef1731dd84e351ce44
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-01-22 00:00:50 +0700

    edit get pet by id

--- api-test/test/get-pet-by-petId.js
+++ api-test/test/get-pet-by-petId.js
@@ -3,8 +3,8 @@ const { getPetByPetId } = require('../api/pet-api.js');
 const testData = require('../data/get-pet-by-petId-data.json');
 
 describe('API Check getPetByPetId', () => {
-    it(`[GET] ${testData.tc_001.tc_title}`, async () => {
-        const response = await getPetByPetId(testData.tc_001.tc_data.petId);
+    it(`${testData.tc_001.tc_title}`, async () => {
+        const response = await getPetByPetId(testData.tc_001.pet_id);
         console.log(response.body);
         expect(response.status).to.equal(testData.tc_001.tc_validator.http_code);
     })
