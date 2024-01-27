ccb2c9b6bde29eb7c59c693d3cdb07efd0a6ca18
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-01-27 21:57:29 +0700

    edit test case

--- api-test/test/end-point/001-get-pet-by-petId.js
+++ api-test/test/end-point/001-get-pet-by-petId.js
@@ -1,11 +1,15 @@
 const expect = require('chai').expect;
 const { getPetByPetId } = require('../../api/pet-api.js');
-const testData = require('../../data/get-pet-by-petId-data.json');
+let testData = require('../../data/get-pet-by-petId-data.json');
 
 describe('API Check getPetByPetId', () => {
-    it(`${testData.tc_001.tc_title}`, async () => {
-        const response = await getPetByPetId(testData.tc_001.pet_id);
-        console.log(response.body);
-        expect(response.status).to.equal(testData.tc_001.tc_validator.http_code);
+    
+    it(`${testData.TC_001.tc_title}`, async function() {
+        if (testData.TC_001.is_run) {
+            const response = await getPetByPetId(testData.TC_001.pet_id);
+            expect(response.status).to.equal(testData.TC_001.tc_validator.http_code);
+        } else {
+            this.skip();
+        }
     })
 })
