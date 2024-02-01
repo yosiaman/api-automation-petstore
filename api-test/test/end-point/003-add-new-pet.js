e38c48c5c54b72b05c2029970cca8054910e7fc6
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-02-01 14:49:10 +0700

    add more checkers and case

--- api-test/test/end-point/003-add-new-pet.js
+++ api-test/test/end-point/003-add-new-pet.js
@@ -1,13 +1,14 @@
 const { expect } = require('chai');
-const { conforms } = require('lodash');
 const { addNewPet, getPetByPetId } = require('../../api/pet-api');
 let testData = require('../../data/003-add-new-pet-data.json');
-const { getPetStatus, getPetCategory, getPetId, getPetName,
-    getPetTags } = require('../../helper/pet-data-generator.js');
+const { getPetStatus, getPetCategory, getPetId, getPetName, getPetTags } = require('../../helper/pet-data-generator.js');
 
 describe('API Check addNewPet', () => {
     // describe data
     let postRequest = {};
+
+    // set delay for each case
+    beforeEach(done => setTimeout(done, 1000));
 
     it(`${testData.TC_001.tc_title}`, async function() {
         if (testData.TC_001.is_run) {
@@ -22,12 +23,23 @@ describe('API Check addNewPet', () => {
 
             // api hit
             const response = await addNewPet(tc_request_body);
+            console.log(response.body);
 
             // response structure checking
             expect(response.status).to.equal(tc_validator.http_code);
             expect(response.body).to.have.keys(tc_validator.parent_keys);
             expect(response.body.category).to.be.an(tc_validator.category_type);
             expect(response.body.tags).to.be.an(tc_validator.tags_type);
+            expect(response.body.photoUrls).to.be.an(tc_validator.photoUrls_type);
+            expect(response.body.id).to.be.an(tc_validator.id_type);
+            expect(response.body.name).to.be.an(tc_validator.name_type);
+            expect(response.body.status).to.be.an(tc_validator.status_type);
+            expect(response.body.category.id).to.be.an(tc_validator.id_type);
+            expect(response.body.category.name).to.be.an(tc_validator.name_type);
+            for (var i=0; i<tc_request_body.tags.length; i++) {
+                expect(response.body.tags[i].id).to.be.an(tc_validator.id_type);
+                expect(response.body.tags[i].name).to.be.an(tc_validator.name_type);
+            }
 
             // response content checking
             expect(response.body.id).to.equal(tc_request_body.id);
@@ -47,18 +59,48 @@ describe('API Check addNewPet', () => {
         }
     })
 
+    // in this cuplicated
+    it(`${testData.TC_002.tc_title}`, async function() {
+        if (testData.TC_002.is_run) {
+            // compose request body
+            let tc_request_body = testData.TC_002.tc_request_body;
+            const tc_validator = testData.TC_002.tc_validator
+            tc_request_body.id = getPetId();
+            tc_request_body.name = getPetName();
+            tc_request_body.status = getPetStatus();
+            tc_request_body.category = getPetCategory();
+            tc_request_body.tags = getPetTags();
+
+            // first api hit & check
+            const response = await addNewPet(tc_request_body);
+            expect(response.status).to.equal(tc_validator.http_code);
+
+            // second api hit & check
+            const response2 = await addNewPet(tc_request_body);
+            expect(response2.status).to.equal(tc_validator.duplicated_http_code);
+
+            // bring to describe data
+            postRequest = tc_request_body;
+        } else {
+            this.skip();
+        }
+    })
+
     afterEach(async function() {
-        // check the data integrity between post and get pet api
-        const response = await getPetByPetId(postRequest.id);
-        expect(response.status).to.equal(200);
-        expect(response.body.id).to.equal(postRequest.id);
-        expect(response.body.name).to.equal(postRequest.name);
-        expect(response.body.status).to.equal(postRequest.status);
-        expect(response.body.category.id).to.equal(postRequest.category.id);
-        expect(response.body.category.name).to.equal(postRequest.category.name)
-        for (var i=0; i<postRequest.tags.length; i++) {
-            expect(response.body.tags[i].id).to.equal(postRequest.tags[i].id);
-            expect(response.body.tags[i].name).to.equal(postRequest.tags[i].name);
+        // check only the passed case
+        if (this.currentTest.state == 'passed') {
+            // check the data integrity between post and get pet api
+            const response = await getPetByPetId(postRequest.id);
+            expect(response.status).to.equal(200);
+            expect(response.body.id).to.equal(postRequest.id);
+            expect(response.body.name).to.equal(postRequest.name);
+            expect(response.body.status).to.equal(postRequest.status);
+            expect(response.body.category.id).to.equal(postRequest.category.id);
+            expect(response.body.category.name).to.equal(postRequest.category.name)
+            for (var i=0; i<postRequest.tags.length; i++) {
+                expect(response.body.tags[i].id).to.equal(postRequest.tags[i].id);
+                expect(response.body.tags[i].name).to.equal(postRequest.tags[i].name);
+            }
         }
 
         // reset used describe data
